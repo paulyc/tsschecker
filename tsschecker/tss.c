bef8186e22d22db396b8c5a2f42ed4ba43563fef
s0uthwest <pashok19967@yandex.ru>
2019-02-10 07:30:19 -0800

    TSS: ignoring 128 error for certain A8(X) devices
    Co-Authored-By: SwiftWinds <swiftwinds@users.noreply.github.com>

--- tsschecker/tss.c
+++ tsschecker/tss.c
@@ -1261,6 +1261,9 @@ char* tss_request_send_raw(char* request, const char* server_url_string, int* re
         } else if (status_code == 126) {
             // An internal error occured, most likely the request was malformed
             break;
+        } else if (status_code == 128) {
+            // Error that occurs when saving blobs on certain A8(X) devices
+            break;
         } else {
             error("ERROR: tss_send_request: Unhandled status code %d\n", status_code);
         }
